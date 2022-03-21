84cd6060e05765eb40a17f2e5bf4aa9951f9f1cc
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2022-03-21 17:53:45 +0300

    build(app): init react-native

---
+++ android/app/src/main/java/com/seed_phrase_example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.seed_phrase_example;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "seed_phrase_example";
+  }
+}
