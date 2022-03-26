54cb101698b6a87bfaca9a177dccb7f257017e4e
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2022-03-26 14:44:38 +0300

    feat(assets): add .svg .png

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import arrow_r from './arrow_r.svg';
+import eye from './eye.svg';
+import eye_closed from './eye_closed.svg';
+import fingerprint from './fingerprint.svg';
+import logo from './logo.svg';
+import circles from './circles.svg';
+
+export default <const>{
+  arrow_r,
+  eye,
+  eye_closed,
+  fingerprint,
+  logo,
+  circles,
+};
