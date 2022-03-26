7ee3a28feb66bc1e697d60976b042942fa26d8b7
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2022-03-26 14:56:01 +0300

    feat(app): + utils

---
+++ src/utils/index.ts
@@ -0,0 +1,13 @@
+
+export function sliceIntoChunks<T>(arr: T[], size: number) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
+}
+
+export function hexAlpha(hex: string, percent: number): string {
+  const str = Math.trunc((percent * 255) / 100).toString(16);
+  return hex + (str.length === 2 ? str : `0${str}`);
+}
