0855c2925e913d3ffc0d7feb02f40baec313d778
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2022-03-26 14:47:00 +0300

    feat(component): add Icon

---
+++ src/components/Icon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SvgCss, XmlProps} from 'react-native-svg';
+import {COLOR} from '../utils';
+import {icons} from '../assets';
+
+type Props = Omit<XmlProps, 'xml'> & {
+  fill?: COLOR;
+  size: number;
+  name: keyof typeof icons;
+};
+export default ({size, name, ...props}: Props) => {
+  const style = {
+    width: size,
+    height: size,
+  };
+  return <SvgCss {...props} {...style} xml={icons[name]} style={[style, props.style]} />;
+};
