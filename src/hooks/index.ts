57ca7112c2ac50c11c55a99e47a122c89c2ac6b5
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2022-03-27 14:06:45 +0300

    feat(hooks): + useModal

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,17 @@
-import {useMemo} from 'react';
-import {Biometric} from '../classes';
+import {useCallback, useEffect, useMemo, useState} from 'react';
+import {Biometric, Seed} from '../classes';
 
 export function useBiometric() {
   return useMemo(() => new Biometric(), []);
+}
+
+export function useModal() {
+  const [modal, setModal] = useState(false);
+
+  const open = useCallback(() => setModal(true), []);
+  const close = useCallback(() => setModal(false), []);
+
+  return [modal, open, close] as const;
 }
 
 export function useSeedPhrase() {
